3e8b9dd00be45022af2d0409b367cdb722182eea
Marcin Kienast <kienastmarcin@gmail.com>
2023-03-23 22:46:41 +0100

    LOOP-2: Extract update and draw functions

--- Loop/src/main.cpp
+++ Loop/src/main.cpp
@@ -14,35 +14,52 @@ struct Triangle
     }
 };
 
+struct GameState
+{
+    float angle = 0.0f;
+};
+
+void update(GameState& gameState)
+{
+    const float degreesPerSecond = 180.0f;
+    float deltaSeconds = GetFrameTime();
+    gameState.angle += degreesPerSecond * deltaSeconds;
+    if (gameState.angle >= 360.0f) {
+        gameState.angle = gameState.angle - 360.0f;
+    }
+}
+
+void draw(const GameState& gameState, const Triangle& triangle)
+{
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    rlPushMatrix();
+    {
+        rlTranslatef(300.0f, 400.0f, 0.0f);
+        rlRotatef(gameState.angle, 0.0f, 0.0f, 1.0f);
+        DrawTriangle(triangle.a, triangle.b, triangle.c, triangle.color);
+    }
+    rlPopMatrix();
+
+    EndDrawing();
+}
+
 int main()
 {
     InitWindow(600, 800, "Loop");
+    SetTargetFPS(60);
 
     Triangle triangle(
             Vector2{-50, 50},
             Vector2{50, 50},
             Vector2{0, -50},
             SKYBLUE);
-    float angle = 0.0f;
 
+    GameState gameState;
     while (!WindowShouldClose()) {
-        angle += 0.1f;
-        if (angle >= 360.0f) {
-            angle = angle - 360.0f;
-        }
-
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        rlPushMatrix();
-        {
-            rlTranslatef(300.0f, 400.0f, 0.0f);
-            rlRotatef(angle, 0.0f, 0.0f, 1.0f);
-            DrawTriangle(triangle.a, triangle.b, triangle.c, triangle.color);
-        }
-        rlPopMatrix();
-
-        EndDrawing();
+        update(gameState);
+        draw(gameState, triangle);
     }
 
     CloseWindow();
